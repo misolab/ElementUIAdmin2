53d09edafa7edbc418b48cf11d80c44393ee87d2
misolab <do.ockhyun@gmail.com>
2019-04-03 08:08:11 +0900

    add balance

--- src/views/_balance/index.vue
+++ src/views/_balance/index.vue
@@ -1,13 +1,147 @@
 <template>
-    <el-button>정산관리</el-button>
+    <div>
+
+        <el-row>
+            <el-col :span="12" style="padding: 10px">
+                <el-card style="border-radius: 0">
+                    <div slot="header">
+                        <el-button style="float: right; position: relative; top: 10px;">완료</el-button>
+                        <span>04/04 정산 금액</span>
+                        <h1>350,000 원</h1>
+                    </div>
+
+                    <div class="demo" style="padding: 10px">
+                        <div style="padding: 10px; margin: 20px 0px 20px 0px;">
+                            <h3>김고객</h3>
+                            <div>
+                                <span style="font-size: large; font-weight: bold">1,000,000 X 10%</span>
+                                <el-input placeholder="+추가정산" style="width: 180px; margin: 0 10px 0 10px;"></el-input>
+                                =
+                                <el-button plain type="primary" style="float: right">100,000 원</el-button>
+                            </div>
+                        </div>
+
+                        <div style="padding: 10px; margin: 20px 0px 20px 0px;">
+                            <h3>박고객</h3>
+                            <div>
+                                <span style="font-size: large; font-weight: bold">5,000,000 X 5%</span>
+                                <el-input placeholder="+추가정산" style="width: 180px; margin: 0 10px 0 10px;"></el-input>
+                                =
+                                <el-button plain type="primary" style="float: right">250,000 원</el-button>
+                            </div>
+                        </div>
+                    </div>
+                </el-card>
+            </el-col>
+
+            <el-col :span="12" style="padding: 10px">
+                <el-card style="border-radius: 0">
+                    <div slot="header">
+                        <span>총 정산금액</span>
+                        <h1>12,345,678,900 원</h1>
+                    </div>
+                    <div class="demo">
+                        <div id="chartPie" style="width:100%; height:400px;"></div>
+
+                        <el-table :data="users" style="width: 100%" @cell-click="dialogTableVisible=true">
+                            <el-table-column label="이름" prop="name" width="150" align="center"></el-table-column>
+                            <el-table-column label="건수" prop="count" width="100" align="center"></el-table-column>
+                            <el-table-column label="정산금액" prop="payment" width="200" align="center"></el-table-column>
+                        </el-table>
+                    </div>
+                </el-card>
+            </el-col>
+        </el-row>
+
+        <el-dialog title="정산 이력" :visible.sync="dialogTableVisible">
+            <el-timeline>
+                <el-timeline-item timestamp="2018/4/12" placement="top">
+                    <el-card>
+                        <h4>김 고객 300,000 원 </h4>
+                        <p>연락처 제공 - 2018/4/2</p>
+                    </el-card>
+                </el-timeline-item>
+                <el-timeline-item timestamp="2018/4/3" placement="top">
+                    <el-card>
+                        <h4>이 고객 10,00,000 원 </h4>
+                        <p>연락처 제공 - 2018/3/30</p>
+                    </el-card>
+                </el-timeline-item>
+                <el-timeline-item timestamp="2018/4/2" placement="top">
+                    <el-card>
+                        <h4>박 고객 1,500,000 원 </h4>
+                        <p>연락처 제공 - 2018/3/30</p>
+                    </el-card>
+                </el-timeline-item>
+            </el-timeline>
+        </el-dialog>
+
+    </div>
+
+
 </template>
 
 <script>
+    import echarts from "echarts";
+
+
     export default {
-        name: "index"
+        name: "index",
+        data() {
+            return {
+                chartPie: null,
+                users:[
+                    {count:33, payment:'335,000', value: 335, name: '김KM'},
+                    {count:31, payment:'310,000', value: 310, name: '한KM'},
+                    {count:23, payment:'234,000', value: 234, name: '도옥현'},
+                    {count:13, payment:'135,000', value: 135, name: '최용일'},
+                    {count:154, payment:'1,548,000', value: 1548, name: '최KM'}
+                ],
+                dialogTableVisible: false,
+            }
+        },
+        methods: {
+            drawPieChart() {
+                this.chartPie = echarts.init(document.getElementById('chartPie'));
+                this.chartPie.setOption({
+                    title: {
+                        text: 'KM별 고객 계약 금액',
+                        subtext: '구성원 수입',
+                        x: 'center'
+                    },
+                    tooltip: {
+                        trigger: 'item',
+                        formatter: "{a} <br/>{b} : {c} ({d}%)"
+                    },
+                    series: [
+                        {
+                            name: '계약 금액',
+                            type: 'pie',
+                            radius: '55%',
+                            center: ['50%', '50%'],
+                            data: this.users,
+                            itemStyle: {
+                                emphasis: {
+                                    shadowBlur: 0,
+                                    shadowOffsetX: 0,
+                                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                                }
+                            }
+                        }
+                    ]
+                });
+            },
+        },
+        mounted: function () {
+            this.drawPieChart()
+        },
+        updated: function () {
+            this.drawPieChart()
+        }
+
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
 </style>
--- src/views/_sales/index.vue
+++ src/views/_sales/index.vue
@@ -1,13 +1,113 @@
 <template>
-    <el-button>영업 관리</el-button>
+  <section>
+    <!-- 툴바 -->
+    <el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
+      <el-form :inline="true" :model="filters">
+        <el-form-item>
+          <el-input v-model="filters.name" placeholder="이름"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="filters.keyman" placeholder="키맨"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-date-picker
+            v-model="filters.date"
+            type="daterange"
+            align="right"
+            unlink-panels
+            range-separator="~"
+            start-placeholder="Start"
+            end-placeholder="End"
+          ></el-date-picker>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" v-on:click="loadData">검색</el-button>
+          <el-button type="warning" v-on:click="loadData">초기화</el-button>
+        </el-form-item>
+        <el-form-item style="float:right;">
+          <el-button type="primary" @click="handleAdd">신규 등록</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+
+    <!-- 테이블 -->
+    <el-table
+      :data="users"
+      highlight-current-row
+      v-loading="listLoading"
+      style="width: 100%;"
+    >
+      <el-table-column prop="sid" label="계약번호" width="100"></el-table-column>
+      <el-table-column prop="regDate" label="계약일" width="120" sortable></el-table-column>
+      <el-table-column prop="name" label="이름" width="100" sortable></el-table-column>
+      <el-table-column prop="payment" label="금액" min-width="100" sortable></el-table-column>
+      <el-table-column prop="rate" label="비율" min-width="100" sortable></el-table-column>
+      <el-table-column prop="keyman" label="키맨" min-width="80" sortable></el-table-column>
+      <el-table-column prop="memo" label="메모" min-width="100"></el-table-column>
+      <el-table-column prop="orgId" label="원계약번호" width="100"></el-table-column>
+
+      <el-table-column label width="150">
+        <template scope="scope">
+          <el-button size="small" @click="handleEdit(scope.$index, scope.row)">편집</el-button>
+          <el-button type="danger" size="small" @click="handleDel(scope.$index, scope.row)">삭제</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <!-- 페이징 -->
+    <el-col :span="24" class="toolbar">
+      <el-pagination
+        layout="prev, pager, next"
+        @current-change="handleCurrentChange"
+        :page-size="20"
+        :total="total"
+        style="float:right;"
+      ></el-pagination>
+    </el-col>
+
+    <!-- 모달창 -->
+  </section>
 </template>
 
 <script>
-    export default {
-        name: "index"
-    }
+export default {
+  name: "sales",
+  data() {
+    return {
+        filters: {
+            name: "",
+            keyman: "",
+            date: ""
+        },
+        users: [],
+        total: 0,
+        page: 1,
+        listLoading: false,
+
+
+
+
+    };
+  },
+  methods: {
+      loadData(){
+          this.total = 1000;
+          this.users = [
+              {sid: 1, regDate: "2018-03-01", name: "김고객",payment: "1,000", rate:"10%", keyman: "최키맨",memo: "", orgId:""},
+              {sid: 2, regDate: "2018-03-10", name: "백고객",payment: "10,000", rate:"5%", keyman: "최키맨",memo: "", orgId:""},
+              {sid: 3, regDate: "2018-03-15", name: "최고객",payment: "3,000", rate:"15%", keyman: "한키맨",memo: "", orgId:""},
+              {sid: 4, regDate: "2018-03-20", name: "김고객",payment: "5,000", rate:"5%", keyman: "최키맨",memo: "금액 변경", orgId:"1번"},
+          ];
+      },
+      handleAdd(){
+
+      },
+  },
+  mounted() {
+      this.loadData()
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
